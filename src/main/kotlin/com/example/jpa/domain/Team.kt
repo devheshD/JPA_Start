94fc35e934739d8b5989caeb74651084ddf2b943
heshAlgo <serverdeveloper@naver.com>
2021-11-04 23:53:35 +0900

    :memo: 연관관계 추가

--- src/main/kotlin/com/example/jpa/domain/Team.kt
+++ src/main/kotlin/com/example/jpa/domain/Team.kt
@@ -14,4 +14,7 @@ data class Team(
     private val id: Long,
 
     private val name: String,
+
+    @OneToMany(mappedBy = "team")       // 반대편 매핑에 어떤것이 걸려있는지 알려주는 것
+    private val members: List<Member>
 )
